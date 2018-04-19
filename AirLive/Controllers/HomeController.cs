5c93fec4c0d7572ce5ab252b7e1410904e92e295
AllenYu <allenyjs@gmail.com>
2018-04-19 18:13:18 +0800

    'version1-2'

--- AirLive/Controllers/HomeController.cs
+++ AirLive/Controllers/HomeController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using AirLive.Models;
+using AirLive.ViewModel;
 
 namespace AirLive.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [OutputCache(Duration =1200, Location = System.Web.UI.OutputCacheLocation.Any)]
+        public async Task<ActionResult> Index()
         {
-            return View();
+            AQIView viewModel = new AQIView();
+            string responseBody = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "http://opendata2.epa.gov.tw/AQI.json";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {}
+                responseBody = await response.Content.ReadAsStringAsync();
+                
+            }
+            ViewBag.responseBody = responseBody;
+            viewModel.AQIs = JsonConvert.DeserializeObject<List<modelAQI>>(responseBody);
+             return View(viewModel);
         }
 
         public ActionResult About()
---
+++ AirLive/ViewModel/AQIView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AirLive.Models;
+
+namespace AirLive.ViewModel
+{
+    public class AQIView
+    {
+        public List<modelAQI> AQIs { get; set; }
+    }
+}
